ee92c36b2e667034ca8dc09f238ec1cb532eb61c
くまとら <ilikehcroy2524@gmail.com>
2022-07-22 07:07:16 +0900

    index画面のスタイルを修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,4 +10,22 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
 end
